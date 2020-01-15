45c15fa599ea10f10d87af724b3b3ccbe6ea7127
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-15 17:52:52 +0000

    Deploy fitnycdigitalinitiatives/onesearch-newbooks to github.com/fitnycdigitalinitiatives/onesearch-newbooks.git:gh-pages

---
+++ alma-new-books.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import urllib
+import os
+import xmltodict
+import pprint
+import json
+
+base_url = "https://api-na.hosted.exlibrisgroup.com/almaws/v1/analytics/reports"
+api_key = os.environ["API_KEY"]
+path = (
+    "/shared/Fashion Institute of Technology 01SUNY_FIT/Reports/20191213-new-book-list"
+)
+params = {"apikey": api_key, "path": path, "limit": "1000", "col_names": "true"}
+url = base_url + "?" + urllib.parse.urlencode(params)
+print(url)
+with urllib.request.urlopen(url) as response:
+    xml = response.read()
+    dict = xmltodict.parse(xml)
+    records = dict["report"]["QueryResult"]["ResultXml"]["rowset"]["Row"]
+    with open("new-books.json", "w") as outfile:
+        json.dump(records, outfile, indent=4)
+    print(str(len(records)) + " results")
+    for record in records:
+        title = record["Column5"]
+        print(title)
