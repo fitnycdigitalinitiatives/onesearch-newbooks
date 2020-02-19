e000c68d6107700c2df8018ebc6114e74d86613d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-19 17:55:21 +0000

    Deploy fitnycdigitalinitiatives/onesearch-newbooks to github.com/fitnycdigitalinitiatives/onesearch-newbooks.git:gh-pages

--- new-books.js
+++ new-books.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-  $.getJSON( "new-books.json", function(data) {
+  $.getJSON("new-books.json", function(data) {
     var slides = [];
     $.each(data, function(i, book) {
       if (book["cover-url"] != "") {
         var slide = `
-        <a class="card" href="` + book["onesearch-url"] + `">
+        <a class="card" target="_top" href="` + book["onesearch-url"] + `">
           <img src="` + book["cover-url"] + `" alt="` + book["title"] + `" class="card-img">
         </a>
         `
@@ -30,22 +30,22 @@ $(document).ready(function() {
     $(".owl-carousel").append(slides);
     var owl = $('.owl-carousel');
     owl.owlCarousel({
-      margin:30,
-      loop:true,
-      autoWidth:true,
-      items:10,
+      margin: 30,
+      loop: true,
+      autoWidth: true,
+      items: 10,
       nav: false,
       dots: false
     });
     // Go to the next item
     $('#next').click(function() {
-        owl.trigger('next.owl.carousel');
+      owl.trigger('next.owl.carousel');
     });
     // Go to the previous item
     $('#previous').click(function() {
-        // With optional speed parameter
-        // Parameters has to be in square bracket '[]'
-        owl.trigger('prev.owl.carousel');
+      // With optional speed parameter
+      // Parameters has to be in square bracket '[]'
+      owl.trigger('prev.owl.carousel');
     });
   });
 });
